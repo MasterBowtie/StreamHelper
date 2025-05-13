4bd4bd2d0d8e0f15e63510d8c6b7a8361fbe2391
MasterBowtie <greenducky12@hotmail.com>
2025-05-13 16:23:41 -0400

    Adjusted Scripture for Intermission

--- client/src/components/Scripture.jsx
+++ client/src/components/Scripture.jsx
@@ -1,6 +1,6 @@
-function Scripture({className}) {
+function Scripture({className, style}) {
     return (
-    <div className={`scripture ${className}`} style={{textAlign: "left"}}>
+    <div className={`scripture ${className}`} style={style}>
         <p>
         7 But ye are commanded in all things to ask of God, who giveth liberally; and that which the Spirit testifies unto you even so I would that ye should do in all holiness of heart, walking uprightly before me, considering the end of your salvation, doing all things with prayer and thanksgiving, that ye may not be seduced by evil spirits, or doctrines of devils, or the commandments of men; for some are of men, and others of devils.
         </p><br/>
--- client/src/pages/Intermission.jsx
+++ client/src/pages/Intermission.jsx
@@ -13,12 +13,24 @@ function Intermission() {
     return (
         <>
             {/* <img className="container" src="src/assets/background_photo.png"></img> */}
-            <video id="introVideo" className="container" autoPlay src="src/assets/Temple_Timelapse_1.mp4" loop type="video/mp4"></video>
+            <video id="introVideo" className="container" autoPlay src="src/assets/Baptistry.mp4" loop type="video/mp4"></video>
             {/* <h3>Starting Soon!</h3> */}
             <Title title={["Please Hold","Madness Happening"]}/>
             <Scripture style={{top: "45%"}}className={"glass"}/>
             {/* <img id="bowtie" src="src/assets/Events.gif" type="image/gif"/> */}
         </>
     )
+
+
+
+
+
+
+
+
+
+
+
+
 }
 export default Intermission
